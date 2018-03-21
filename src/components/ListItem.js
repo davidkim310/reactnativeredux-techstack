938c9aa27662412cf4d37f886ffb3a8d13703af2
David Kim <davidkim310@gmail.com>
2018-03-21 16:49:04 -0700

    adds dropdown for more info

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+    Text,
+    TouchableWithoutFeedback,
+    View,
+    LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
-  render() {
-    const { titleStyle } = styles;
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+    renderDescription() {
+        const { library, expanded } = this.props;
 
-    return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
-    );
-  }
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={{ flex: 1 }}>{library.description}</Text>
+                </CardSection>
+            );
+        }
+    }
+
+    render() {
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library;
+
+
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
 }
 
 const styles = {
-  titleStyle: {
-    fontSize: 18,
-    paddingLeft: 15
-  }
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id
+    return { expanded: expanded }
+};
+
+//first argument "null" is for mapStateToProps
+export default connect(mapStateToProps, actions)(ListItem);
